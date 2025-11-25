bf137deb73f068675539e31ca92f67068c52ee19
James McDonald <jamesmcdonald112@yahoo.ie>
2025-11-25 16:00:14 +0000

    feat(sentry): add client/server SDK setup and integrate Sentry into Astro project

--- astro.config.ts
+++ astro.config.ts
@@ -5,11 +5,16 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import sentry from "@sentry/astro";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
-	},
+    plugins: [tailwindcss()],
+  },
 
-  integrations: [react()],
+  integrations: [
+    react(),
+    sentry(),
+  ],
 });
---
+++ sentry.server.config.js
@@ -0,0 +1,14 @@
+import * as Sentry from "@sentry/astro";
+
+Sentry.init({
+	dsn: "https://e7f574cecec4aef790910d5ec70f93cb@o4510425283231744.ingest.de.sentry.io/4510426322370640",
+	// Adds request headers and IP for users, for more info visit:
+	// https://docs.sentry.io/platforms/javascript/guides/astro/configuration/options/#sendDefaultPii
+	sendDefaultPii: false,
+
+	tracesSampleRate: 0,
+
+	org: "james-mcdonald",
+	project: "javascript-astro-7j",
+	authToken: process.env.SENTRY_AUTH_TOKEN,
+});
