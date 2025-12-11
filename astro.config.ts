365e6998d62be585ce0467b213ec8cc72aa19607
James McDonald <jamesmcdonald112@yahoo.ie>
2025-12-11 20:15:16 +0000

    chore: update Astro config and dependency changes for Vercel deploy

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,15 @@
 // @ts-check
 
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 import sentry from "@sentry/astro";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	output: "server",
+	adapter: vercel(),
 	vite: {
 		plugins: [tailwindcss()],
 	},
