9ffa069cf2e9b4bc73cddf7288e82b36028aae11
James McDonald <jamesmcdonald112@yahoo.ie>
2025-12-12 16:57:03 +0000

    fix(sentry): remove server-only options from client config

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/astro";
 
 Sentry.init({
-	dsn: "https://e7f574cecec4aef790910d5ec70f93cb@o4510425283231744.ingest.de.sentry.io/4510426322370640",
+	dsn: import.meta.env.PUBLIC_SENTRY_DSN,
 	// Adds request headers and IP for users, for more info visit:
 	// https://docs.sentry.io/platforms/javascript/guides/astro/configuration/options/#sendDefaultPii
 	sendDefaultPii: false,
@@ -9,8 +9,4 @@ Sentry.init({
 	tracesSampleRate: 0, // disable tracing unless needed
 	replaysSessionSampleRate: 0,
 	replaysOnErrorSampleRate: 0,
-
-	org: "james-mcdonald",
-	project: "javascript-astro-7j",
-	authToken: process.env.SENTRY_AUTH_TOKEN,
 });
