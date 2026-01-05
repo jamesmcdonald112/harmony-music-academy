e2f083ca7063d55b4d0a18a834787d0dc9c0eaeb
James McDonald <jamesmcdonald112@yahoo.ie>
2026-01-05 22:36:46 +0000

    refactor(infopack): extract sendInfoPackHandler and delegate to deliverInfoPack

---
+++ src/features/InfoPack/service/deliverInfoPack.ts
@@ -0,0 +1,10 @@
+export async function deliverInfoPack(input: {
+	fullName: string;
+	email: string;
+	phone: string;
+	studentAge?: number;
+	message?: string;
+}): Promise<void> {
+	// TODO: Implement email sending (Resend, SMTP, etc.)
+	void input;
+}
